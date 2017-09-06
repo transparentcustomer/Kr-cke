d0a0f8a1f1f94f87a4770894bf48cdb7bf565141
transparentcustomer <transparentcustomer@gmail.com>
2017-09-06 13:57:16 +0200

    progress with downloading the company name

--- stockFish/stockBrain.swift
+++ stockFish/stockBrain.swift
@@ -8,30 +8,36 @@
 
 import Foundation
 
+
+
 struct stockBrain{
     
-    private var yahooSymbol: String?
-    var stockSymbolArray = [String]()
-    var yahooStockDataArray = [[String:String]]()
+    var stockPuller = yahooPuller()
+    
+    private var yahooSymbol:    String?
+    var stockSymbolArray    =   [String]()
+    var yahooStockDataArray =   [[String:String]]()
     
     var stockTableViewData = [[String:String]]()
     
-    //FIXME: ..toDelete
+    //FIXME: ..toDelete:
     /*
-         mutating func getStockValues (_ stockCode: String?){
-             yahooSymbol = stockCode
+     mutating func getStockValues (_ stockCode: String?){
+     yahooSymbol = stockCode
      
-             if stockCode != "" {
-                 stockSymbolArray.insert( (yahooSymbol ?? "no symb found"), at: 0) //.. nil-coalescing operator
-                 print("shit")
+     if stockCode != "" {
+     stockSymbolArray.insert( (yahooSymbol ?? "no symb found"), at: 0) //.. nil-coalescing operator
+     print("shit")
      
-             }else{print("no symbol inserted")}
+     }else{print("no symbol inserted")}
      
      
-             print("stockSymbolArray: \(stockSymbolArray)")
-         }
+     print("stockSymbolArray: \(stockSymbolArray)")
+     }
      */
     
+    
+    //MARK: - filling of model with symbols
     mutating func fillStockArray(_ stockCode: String) -> [[String:String]] {
         print("test")
         yahooStockDataArray.insert([
@@ -47,7 +53,41 @@ struct stockBrain{
     
     
     //MARK: - get Yahoo Info:
-    func getUpdates(){
+    func getUpdates()
+    { //.. to get stock names and price values
         print("yahooStockDataArray: \(yahooStockDataArray)")
+        
+        
+        
+        for (index,var item) in yahooStockDataArray.enumerated()
+        {
+            //FIXME: ..toDelete
+            //print("the index is: \(index) and the content: \(item["code"]!)")
+            
+            //FIXME:  .. beware of cycling //.. in the case of no stock belonging to a symbol
+            
+            let symbol = item["code"]!
+            
+            //MARK: get the name
+            if item["name"] == "no info" {
+                
+                let diagnose =  stockPuller.getStockName(yahoosymbol: symbol)
+                
+                print("\(String(describing: item["code"]!)) has no name yet")
+            }
+            
+            
+            
+            
+            
+            
+            //FIXME: ..toDelete
+            //print("Diagnose: \(diagnose)")
+            
+        }
+        
     }
+    
+    
+    
 }
---
+++ stockFish/yahooStockPuller.swift
@@ -0,0 +1,164 @@
+//
+//  yahooStockPuller.swift
+//  stockFish
+//
+//  Created by THECAT on 06.09.17.
+//  Copyright © 2017 THECAT. All rights reserved.
+//
+
+import Foundation
+
+
+struct yahooPuller{
+
+    
+    func getLastPrice (){
+    /*{print(#function, #line, "for:", portGetOfK("code"))
+        
+        print("indexIndiStock: \(indexIndiStock)", #line)
+        
+        let letzterPreis = (portfolioData.object(at: indexIndiStock) as AnyObject).value(forKey: "lastprice")
+        let quantity = (portfolioData.object(at: indexIndiStock) as AnyObject).value(forKey: "quantity")
+        print(quantity == nil)
+        print("letzter Preis: \(String(describing: letzterPreis)) vs", #line)
+        //        print("portGetOfK(lastprice): \(portGetOfK("lastprice").isEmpty) ", #line)
+        
+        ///last stock price of a chosen symbol (with l1)
+        if letzterPreis == nil && loadDefaults.isEmpty == true {
+            
+            print("--01--there was no last price yet",#line)
+            
+            let name = "\(getDreamValue(valuecode: formCoDict!["name"]! as! String))"
+            guard name != "N/A\n"   else { throw ErrorHandling.stockError.wrongSymbol }
+            
+            let lastprice = "\(getDreamValue(valuecode: formCoDict!["lastprice"]! as! String))"
+            guard lastprice != "N/A\n"   else { throw ErrorHandling.stockError.stockExchangeClosed }
+            
+            let selectedDict = portfolioData.object(at: indexIndiStock) as! Dictionary<String, String>
+            
+            for pair in (selectedDict) { wortbuch.updateValue(pair.value, forKey: pair.key) }
+            
+            updateWortbuch(wantedvalueArray: ["name","currency","lastprice"])
+            
+            wortbuch.updateValue(dateModulator(), forKey: "lastupdate")
+            
+            print(#function,#line)
+            //            calcNumbers()
+            
+            portfolioData.removeObject(at: indexIndiStock)
+            portfolioData.insert(wortbuch, at: indexIndiStock)
+            wortbuch.removeAll()
+            
+            
+            
+            print("brain.loadDefaults.isEmpty: \(loadDefaults.isEmpty)", #line)
+            
+            
+        }
+            
+            //        else if letzterPreis == nil && loadDefaults.isEmpty == true && quantity == nil
+            //        {print("--03--load new symbols",#line)
+            //
+            //            //
+            //        }
+        else if portGetOfK("lastprice") != "\(updateDefaults(stockSymbol: portGetOfK("code"), wantedvalue: "lastprice"))"
+        {print("--02--",#line)
+            
+            
+            //            let name = "\(getDreamValue(valuecode: formCoDict!["name"]! as! String))"
+            //            guard name != "N/A\n"   else { throw errorHandling.stockError.wrongSymbol }
+            
+            
+            if (loadDefaults.isEmpty)
+                
+            {
+                print("")
+                print("load defaults is empty", #line)
+                
+                let selectedDict = portfolioData.object(at: indexIndiStock) as! Dictionary<String, String>
+                for pair in (selectedDict) { wortbuch.updateValue(pair.value, forKey: pair.key) }
+                
+                wortbuch.updateValue("\(updateDefaults(stockSymbol: portGetOfK("code"), wantedvalue: "lastprice"))", forKey: "lastprice")
+                wortbuch.updateValue(dateModulator(), forKey: "lastupdate")
+                
+                print(#function,#line)
+                calcNumbers()
+                
+                portfolioData.removeObject(at: indexIndiStock)
+                portfolioData.insert(wortbuch, at: indexIndiStock)
+                
+            }else{
+                print("load defaults is not empty", #line)
+                //                print("loadDefaults:\(loadDefaults)")
+                portfolioData.removeAllObjects()
+                //                print("loadDefaults:\(loadDefaults)")
+                for (index,var item) in (loadDefaults.enumerated())
+                {
+                    let updatedprice = "\(updateDefaults(stockSymbol: item["code"]! , wantedvalue: "lastprice"))"
+                    item.updateValue(updatedprice, forKey: "lastprice")
+                    item.updateValue(dateModulator(), forKey: "lastupdate")
+                    _ = [item]
+                    portfolioData.insert(item, at: index)
+                    print("line:", #line, "- brain.portfolioData: \(portfolioData)" )
+                    //                    calcNumbers()
+                }
+            }
+        }
+            
+            
+            
+            
+            
+        else if letzterPreis != nil && loadDefaults.isEmpty == true
+        {
+            print("")
+        }else{
+            print("no way")
+        }
+        
+        
+        
+        ///shows the last update of the last price
+        if (portfolioData.object(at: indexIndiStock) as AnyObject).value(forKey: "lastupdate") == nil{
+            setLast(object: dateModulator(),                    key: "lastupdate")
+            //        symbolArray["lastprice"]?.append(portfolioData[indexIndiStock].objectForKey("lastprice")! as! String)
+        }else
+        {
+            
+        }
+        
+        }*/
+    }
+    
+    func getStockName(yahoosymbol: String) -> String{
+        
+        var stockName:String?
+        
+        stockName =  yahoosymbol
+        
+        var nameURL = completeYahooURL(yahoosymbol)
+        
+        print("URL to get the name for: \(yahoosymbol): \(nameURL)")
+        
+        
+        
+        
+        return stockName!
+    }
+    
+    
+    //MARK: format yahoo link
+    func completeYahooURL (_ symbol: String)->String
+        
+    {print(#function, #line)
+        
+        let symbolLetter = "n"
+        let finishedYahooURL =  "http://download.finance.yahoo.com/d/quotes.csv?s=\(symbol)&f=\(symbolLetter)"
+
+        
+        return finishedYahooURL
+    }
+    
+    
+    
+}
