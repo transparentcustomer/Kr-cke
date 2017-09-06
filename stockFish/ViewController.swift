62d3d688a9d4f35c024fef4e2966d05f7bc66c2c
transparentcustomer <transparentcustomer@gmail.com>
2017-09-06 08:06:09 +0200

    Initial Commit

---
+++ stockFish/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  stockFish
+//
+//  Created by THECAT on 06.09.17.
+//  Copyright © 2017 THECAT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
